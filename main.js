7982a6cba91dd03e4b181d4f4951b9076e3b4660
Ivan Vizcarra <ivanvizcarra99@gmail.com>
2023-10-20 12:14:26 -0600

    Deploy app

---
+++ main.js
@@ -0,0 +1,52 @@
+let sideMenu = document.querySelectorAll(".nav-link");
+sideMenu.forEach(item =>{
+  let li = item.parentElement;
+  item.addEventListener("click", () =>{
+      sideMenu.forEach(link =>{
+        link.parentElement.classList.remove("active");
+      });
+      li.classList.add("active");
+  });
+});
+
+let menuBar = document.querySelector(".menu-btn");
+let sideBar = document.querySelector(".sidebar");
+
+menuBar.addEventListener("click", ()=>{
+  sideBar.classList.toggle("hide");
+});
+
+let switchMode = document.querySelector("#switch-mode");
+switchMode.addEventListener("change",(e)=>{
+  if(e.target.checked){
+    document.body.classList.add("dark");
+  }else{
+    document.body.classList.remove("dark");
+  }
+});
+
+let searchForm = document.querySelector(".content nav form");
+let searchBtn = document.querySelector(".search-btn");
+let searchIcon = document.querySelector(".search-icon");
+
+searchBtn.addEventListener("click", (e) =>{
+  if(window.innerWidth < 576){
+    e.preventDefault();
+    searchForm.classList.toggle("show");
+    if(searchForm.classList.contains("show")){
+      searchIcon.classList.replace("fa-search", "fa-times");
+    }else{
+      searchIcon.classList.replace("fa-times", "fa-search");
+    }
+  }
+});
+
+window.addEventListener("resize", () =>{
+  if(window.innerWidth > 576){
+    searchIcon.classList.replace("fa-times", "fa-search");
+    searchForm.classList.remove("show")
+  }
+  if(window.innerWidth < 768){
+    sideBar.classList.add("hide");
+  }
+});
